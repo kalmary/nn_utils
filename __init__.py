6dc977d16f345661ae573ac8c8336e9cfad03307
kalmary <58998639+kalmary@users.noreply.github.com>
2025-11-28 13:47:18 +0100

    imports update

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-from src.accuracy_metrics import *
-
-from src.evaluation_plot_tools import *
-from src.file_handling import *
-
-from src.loss_functions import *
+from .src.accuracy_metrics import *
+from .src.evaluation_plot_tools import *
+from .src.file_handling import *
+from .src.loss_functions import *
